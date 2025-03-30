0c4633594d744d586acca1669d5f3f7503716bd5
Theo Bikoko <25634122r@iespoblenou.org>
2025-03-30 23:47:05 +0200

    Initial commit

---
+++ src/Hero/Item.java
@@ -0,0 +1,32 @@
+package Hero;
+
+public class QuestData {
+
+    private String itemName;
+    private String itemKind;
+    private int itemPower;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemKind() {
+        return itemKind;
+    }
+
+    public void setItemKind(String itemKind) {
+        this.itemKind = itemKind;
+    }
+
+    public int getItemPower() {
+        return itemPower;
+    }
+
+    public void setItemPower(int itemPower) {
+        this.itemPower = itemPower;
+    }
+}
